d50ca711defcfb21564280e8663caa507c64db30
Francisco Casillas <chiccasillas@gmail.com>
2025-02-04 10:50:11 -0600

    Initial Commit

---
+++ FirstLayout/FirstLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstLayoutApp.swift
+//  FirstLayout
+//
+//  Created by Francisco Jean on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
